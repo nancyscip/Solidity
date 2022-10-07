bb8b67609bb3c6de1e0c86f1fac601613797fb07
Nancy Scipioni <100371567+nancyscip@users.noreply.github.com>
2022-10-06 23:01:11 -0300

    Update SistemaVotacion.sol

--- SistemaVotacion.sol
+++ SistemaVotacion.sol
@@ -1,51 +1,82 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-contract Voting{
+contract VotingSystem{
 
-    address public commission;
+    struct candidates{
+        uint8 id;
+        string name;
+        string party;
+        uint256 votes;
+    }
 
-    uint public startTime;
-    uint public endTime;
+    struct Voters{
+     
+        bool registeredVote;
+        bool canVote;
+    }
 
-    bytes32 [] public voters;
-    mapping(bytes32 => bool) votersExists;
+    struct VotingTime{
+        uint startTime; 
+        uint endTime; 
+    }
+
+    mapping(address => Voters) public voters;
+
+    mapping(uint256 => bool) public votersExists;
+
+    mapping(uint256 => bool) hasVoted;
+
+    candidates public c1;
+    candidates public c2;
+
+    /*Voters public v1;*/
+
+    VotingTime public vT;
 
     bool internal isVoterVoting;
-    
-    int [] public votes;
-    mapping(bytes32 => bool) hasVoted;
-    
+
     bool internal isVotesCounted;
+
+    bool BallotBoxAdressEnable = false;
+
+    address public owner;
+
+    constructor(
+         string memory _name, string memory _party, uint8 _id, string memory _name2, string memory _party2, uint8 _id2){
+        owner = msg.sender;
+       
+
+        c1.name = _name;
+        c1.party = _party;
+        c1.id = _id;
+
+        c2.name = _name2;
+        c2.party = _party2;
+        c2.id =_id2;
+    }
+
+    // The modifier that allows only the election owner to access the functions
+    modifier onlyElectionOwner() {
+        require(msg.sender == owner,"Only the election owner can access this function.");
+        _;
+    }
+
+      function StartVoting() public onlyElectionOwner(){ 
     
-    mapping(int => int) partyVotes;
-
-    // The constructor to save the address of the election commission, start time and end tine.
-    constructor(uint _startTime, uint _endTime){
-        commission = msg.sender;
-        startTime = _startTime;
-        endTime = _endTime;
+        vT.startTime = block.timestamp; 
+        
     }
 
-    // The modifier that allows only the election commission to access the functions
-    modifier onlyElectionCommission() {
-        require(msg.sender == commission,"Only the election commission can access this function.");
-        _;
+    function FinishVoting() public onlyElectionOwner(){
+        vT.endTime = block.timestamp; 
+        BallotBoxAdressEnable = false; 
     }
 
-    // The modifier that allows only the voters to vote between the start time and the end time. It also allows only one voter to vote at a time.
-    modifier allowVoting() {
-        require(startTime <= block.timestamp, "You can not vote now. The voting has not started yet.");
-        require(endTime >= block.timestamp, "You can not vote now. The voting has been compleated.");
-        require(!isVoterVoting,"Some one else is voting. Please wait and try again.");
-        isVoterVoting = true;
-        _;
-        isVoterVoting = false;
-    }
-
-    // The modifier that allows to count vote only after the end time
+     // The modifier that allows to count vote only after the end time
     modifier onlyAfterEndTime() {
-        require(endTime <= block.timestamp, "Counting can not be started now.");
+        require(vT.endTime <= block.timestamp, "Counting can not be started now.");
         isVotesCounted = true;
         _;
     }
@@ -56,38 +87,31 @@ contract Voting{
         _;
     }
 
-    // The function to create a voter by the election commission
-    function createVoter(string memory voterId) public onlyElectionCommission {
-        bytes32 userId = sha256(abi.encodePacked(voterId));
-        require(!votersExists[userId], "Account has already been created.");
-        votersExists[userId] = true;
-        voters.push(userId);
+   
+
+     function giveRightToVote(address voter) public onlyElectionOwner{
+        require(owner == msg.sender, "No eres el INE DEX");
+        voters[voter].canVote = true;
     }
 
-    // The function to vote by the voters
-    function vote(string memory voterId, int partyNumber) public allowVoting {
-        bytes32 userId = sha256(abi.encodePacked(voterId));
-        require(votersExists[userId], "This voter is not atunticated by the election commission.");
-        require(!hasVoted[userId], "This voter has already voted.");
-        hasVoted[userId] = true;
-        votes.push(partyNumber);
-    }
-    
-    // The function to count the number of votes secured by each party
-    function countVotes() public onlyAfterEndTime{
-        uint indexVotes = 0;
-        uint numberOfVotes = votes.length;
-        while (numberOfVotes != 0) {
-            int currentVote = votes[indexVotes];
-            partyVotes[currentVote] = partyVotes[currentVote] + 1;
-            indexVotes++;
-            numberOfVotes--;
-        }
-    }
 
-    // The function to view the resluts of a particular party
-    function viewResultsOf(int partyNumber) public view onlyAfterCountingVotes returns(int){
-        return partyVotes[partyNumber];
-    }
+    /*/ The function to vote by the voters
+    function vote(uint256 voterId, uint8 idCandidato) public {
+        uint256 id = voterId;
+        require(votersExists[id], "This voter is not atunticated by the election owner.");
+        require(!hasVoted[id], "This voter has already voted.");
+        hasVoted[id] = true;
+         if(idCandidato == c1.id){
+           c1.votes += 1;
+       } else{
+           c2.votes += 1;
+       }*/
 
-}
+        function addvoter(address _voters, uint256 _voterId) public  {
+        Voters memory v1;
+        v1.voterId = _voterId;
+        v1.hasVoted = false;
+        
+        
+    }
+    }
